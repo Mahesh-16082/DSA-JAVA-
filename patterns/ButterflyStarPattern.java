b6fc4be2308890dad1edc42dbf6b92ba339d9b06
Mahesh-16082 <maheshseereddy@gmail.com>
2025-06-30 15:54:16 +0530

    modify

--- patterns/ButterflyStarPattern.java
+++ patterns/ButterflyStarPattern.java
@@ -15,7 +15,7 @@ public class ButterflyStarPattern {
             }
             System.out.println();
         }
-        for(int i=4;i>=1;i--){
+        for(int i=n-1;i>=1;i--){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
